333183bb98e999ded346836c4b28ab8119f4f1ee
ErciNguyen <88387748+ErciNguyen@users.noreply.github.com>
2021-08-13 22:01:38 +0700

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ const CONFIG = {
     desc: 'Phải chăng anh đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Vẫn cứ là thích em <33',
     btnNo: 'Không, chàng trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao tớ lại yêu em <3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao tớ lại yêu em  <3',
     btnReply: 'Gửi cho em  <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',

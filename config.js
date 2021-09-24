a2156d9455dbf1d1093718b935f1efd8a83d31d3
ErciNguyen <88387748+ErciNguyen@users.noreply.github.com>
2021-09-24 10:02:57 +0700

    vvv

--- config.js
+++ config.js
@@ -1,16 +1,16 @@
 const CONFIG = {
-    introTitle: ' Em à!',
-    introDesc: `Anh đã yêu em từ bao giờ.Anh cũng không biết nữa`,
+    introTitle: ' Nhi à!',
+    introDesc: `Chúng ta quen nhau từ bao giờ nhỉ.Chú không biết nữa`,
     btnIntro: 'hihi',
-    title: 'làm thế nào để anh ngừng yêu em',
-    desc: 'Phải chăng anh đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích em <33',
-    btnNo: 'Không, chàng trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao tớ lại yêu em  <3',
-    btnReply: 'Gửi cho em  <3',
+    title: 'Làm sao để giận Nhi, Nhi đáng yêu quá trời lun!',
+    desc: 'Phải chăng Chú đã say ngay từ lúc thấy sự dễ thương ấy ',
+    btnYes: 'Vẫn cứ là thích Chú <33',
+    btnNo: 'Không, Chú à :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao Chú lại gặp được Nhi  <3',
+    btnReply: 'Gửi cho Nhi  <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Anh muốn nói.....Anh yêu em',
-    btnAccept: 'Em cũng yêu anh iuiu ',
-    messLink: 'https://www.facebook.com/nguyenss.nam.75' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    mess: 'Anh biết mà 🥰. Thương Nhi nhiều nhiều 😘😘',
+    messDesc: 'Chú muốn nói.....Nhi đáng ghét!!!!, đáng yêu😘😘😘',
+    btnAccept: 'Nhi cũng yêu Chú iuiu ',
+    messLink: 'https://www.facebook.com/kulls.links' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }

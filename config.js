ea066b1643ce2b00cf80581a5d6b50c095d0a6f8
ErciNguyen <88387748+ErciNguyen@users.noreply.github.com>
2021-09-24 10:22:23 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ const CONFIG = {
     introDesc: `Chúng ta quen nhau từ bao giờ nhỉ.Chú không biết nữa`,
     btnIntro: 'hihi',
     title: 'Làm sao để giận Nhi, Nhi đáng yêu quá trời lun!',
-    desc: 'Phải chăng Chú đã say ngay từ lúc thấy sự dễ thương ấy ',
+    desc: 'Phải chăng Chú đã say ngay từ lúc thấy sự dễ thương ấy. ',
     btnYes: 'Vẫn cứ là thích Chú <33',
     btnNo: 'Không, Chú à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao Chú lại gặp được Nhi  <3',

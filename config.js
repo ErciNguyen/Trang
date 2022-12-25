c763f2a1ad58df22a31d39dac2aa6a57cfad01f4
ErciNguyen <88387748+ErciNguyen@users.noreply.github.com>
2022-12-26 00:05:24 +0700

    Gianganh

--- config.js
+++ config.js
@@ -1,16 +1,16 @@
 const CONFIG = {
-    introTitle: ' Chị à!',
+    introTitle: ' Giang Anh!',
     introDesc: `Chúng ta biết nhau được bao lâu rui nhỉ. Em không biết nữa.`,
     btnIntro: 'hihi',
     title: 'I want to see you every day',
-    desc: 'Phải chăng Văn đã say ngay từ lúc thấy sự dễ thương ấy. ',
+    desc: 'Phải chăng anh đã say ngay từ lúc thấy sự dễ thương ấy. ',
     btnYes: 'Oke nè. <33',
     btnNo: 'Không nha :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao Văn lại gặp được Trang  <3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao Văn lại gặp được Giang Anh  <3',
     btnReply: 'Gửi cho Em  <<3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Chị cho em cơ hội theo đuổi chị nha',
-    messDesc: 'Em .....nghiêm túc',
-    btnAccept: 'Chị biết rui',
+    mess: 'Em cho anh theo đuổi em  nha',
+    messDesc: 'Anh nghiêm túc',
+    btnAccept: 'Em biết rui',
     messLink: 'https://www.facebook.com/kulls.links' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }

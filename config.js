3c47154994e3a59ec595e0c577db31781cd582f8
ErciNguyen <88387748+ErciNguyen@users.noreply.github.com>
2022-12-26 00:13:07 +0700

    ggg

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ const CONFIG = {
     desc: 'Phải chăng anh đã say ngay từ lúc thấy sự dễ thương ấy. ',
     btnYes: 'Oke nè. <33',
     btnNo: 'Không nha :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao Văn lại gặp được Giang Anh  <3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao anh lại gặp được Giang Anh  <3',
     btnReply: 'Gửi cho Em  <<3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Em cho anh theo đuổi em  nha',
